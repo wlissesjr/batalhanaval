f14d95383845b41cdae917a0cac961019953ced5
Wlisses Guerra <wlissesguerra@hotmail.com>
2015-06-15 18:53:34 -0300

    nao sei

--- bala.cpp
+++ bala.cpp
@@ -9,17 +9,19 @@ using namespace std;
 Bala::Bala(float origem_x, float origem_y, float destino_x, float destino_y, int tipo){
 	this->posicao_x = origem_x+16;
 	this->posicao_y = origem_y;
+
 	float cateto_x = destino_x - origem_x;
 	float cateto_y = destino_y - origem_y;
-	float hipotenuza = sqrt((cateto_x*cateto_x)+(cateto_y*cateto_y));
+
 	if(tipo == 1){
-		this->velocidade_x = 0;
-		this->velocidade_y = -(hipotenuza/60);
+		this->velocidade_x = cateto_x/6;
+		this->velocidade_y = cateto_y/6;
 		this->atrito = 0;
 	}
 }
 
 void Bala::Render(){
 	ALLEGRO_COLOR cor = al_map_rgb(255,0,0);
+	
 	al_draw_filled_circle(this->posicao_x, this->posicao_y, 3, cor);
 }
